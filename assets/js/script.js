25b2d3e2b75d8557df8f538cabffd0b321a8075c
Imoh Henry <henryimoh2@gmail.com>
2024-01-16 14:28:17 +0100

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,8 +20,28 @@ $.fn.jQuerySimpleCounter = function( options ) {
   };
   
   $('#number1').jQuerySimpleCounter({end: 583,duration: 3000});
-  $('#number2').jQuerySimpleCounter({end: 946,duration: 700});
+  $('#number2').jQuerySimpleCounter({end: 946,duration: 5000});
   $('#number3').jQuerySimpleCounter({end: 81,duration: 3100});
   $('#number4').jQuerySimpleCounter({end: 16,duration: 3450});
 //   $('#number5').jQuerySimpleCounter({end: 472,duration: 3300});
 //   $('#number6').jQuerySimpleCounter({end: 224,duration: 2175});
+
+
+function dropFunction() {
+    var dropdown = document.getElementById("myDropdown");
+    dropdown.classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function (event) {
+    if (!event.target.closest('.drop-parent')) {
+      var dropdowns = document.getElementsByClassName("dropdown-list");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
